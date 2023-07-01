8bc50a9b1f9d95ef8782320a225fa3fb9ea10bd4
yue.zhang <zhangyue9306@gmail.com>
2023-07-01 11:48:25 +0800

    add load accounts

--- eth-todo-list/src/app.js
+++ eth-todo-list/src/app.js
@@ -4,6 +4,7 @@ App = {
 
     load: async () => {
         await App.loadWeb3()
+        await App.loadAccount()
     },
 
     // https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
@@ -14,7 +15,7 @@ App = {
                 // Request account access if needed
                 await ethereum.enable()
                 // Accounts now exposed
-                web3.eth.sendTransaction({/* ... */})
+                // web3.eth.sendTransaction({/* ... */})
               } catch (error) {
                 window.alert("User denied account access...")
               }
@@ -22,13 +23,19 @@ App = {
         // Legacy dapp browsers...
         else if (window.web3) {
             window.web3 = new Web3(web3.currentProvider);
-            // Acccounts always exposed
-            web3.eth.sendTransaction({/* ... */});
+            // Accounts always exposed
+            // web3.eth.sendTransaction({/* ... */});
         }
         // Non-dapp browsers...
         else {
             console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
         }
+    },
+
+    loadAccount: async () => {
+        let accounts = await web3.eth.getAccounts()
+        App.account = accounts[0]
+        console.log(App.account)
     }
 }
 
