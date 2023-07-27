fa4cec5c89fd1a7dcf17cc6dc19a37481d628024
yue.zhang <zhangyue9306@gmail.com>
2023-07-27 10:57:32 +0800

    add test mine blockchain

--- blockchain/test_blockchain_impl.py
+++ blockchain/test_blockchain_impl.py
@@ -3,7 +3,7 @@ import time
 from unittest import TestCase
 from typing import List
 from blockchain_impl import Block, Transaction, Wallet, verify
-from blockchain_impl import BlockChain
+from blockchain_impl import BlockChain, GenesisPublicKey
 import secrets
 from exceptions import *
 
@@ -38,6 +38,67 @@ def generate_signed_transaction(wa: Wallet, wb: Wallet, amount: int = 0, ts=None
     )
     signed_txn = wa.sign(unsigned_txn)
     return signed_txn
+
+
+def generate_blockchain(length: int, n_transactions: int = 2, n_users: int = 3, with_pending: bool = False):
+    wallets = [Wallet() for _ in range(n_users)]
+    accounts = [100 for _ in range(n_users)]
+    miner_addr = wallets[0].public_key
+
+    def _get_transactions():
+        nonlocal n_transactions, n_users
+        txns = []
+        for _ in range(n_transactions):
+            sender_idx = random.randint(0, n_users - 1)
+            while accounts[sender_idx] < 1:
+                sender_idx = random.randint(0, n_users)
+            receiver_idx = (sender_idx + 1) % n_users
+            amount = random.randint(1, accounts[sender_idx] - 1)
+            txn = generate_signed_transaction(
+                wallets[sender_idx],
+                wallets[receiver_idx],
+                amount=amount
+            )
+            txns.append(txn)
+            accounts[sender_idx] -= amount
+            accounts[receiver_idx] += amount
+        return txns
+
+    prev_block = None
+    chain = []
+    first = True
+    while length > 0:
+        if first:
+            block = Block(
+                prev_block=prev_block,
+                transactions=[Transaction(
+                    from_addr=GenesisPublicKey(None),
+                    to_addr=wallets[i].public_key,
+                    amount=100
+                ) for i in range(n_users)],
+                reward=10,
+                difficulty=1
+            )
+        else:
+            block = Block(
+                prev_block=prev_block,
+                transactions=_get_transactions(),
+                reward=10,
+                difficulty=1
+            )
+        if not first:
+            block.mine(miner_addr)
+        if prev_block is not None:
+            prev_block.next_block = block
+        chain.append(block)
+        prev_block = block
+        length -= 1
+        first = False
+    return BlockChain(
+        chain=chain,
+        reward=10,
+        difficulty=1
+    ), accounts, wallets
 
 
 class TestWallet(TestCase):
@@ -237,22 +298,31 @@ class TestBlockChain(TestCase):
         pass
 
     def test_mine_succeed_with_valid_block(self):
-        txn1 = generate_signed_transaction(self.wc, self.wa, 30)
-        txn2 = generate_signed_transaction(self.wa, self.wb, 10)
-        prev_block = Block(
-            prev_block=None,
-            transactions=[txn1],
-            reward=self.reward,
-            difficulty=self.difficulty
+        blockchain, accounts, wallets = generate_blockchain(length=3, n_transactions=1, n_users=3)
+        blockchain.add_transaction(
+            generate_signed_transaction(
+                wallets[0],
+                wallets[1],
+                amount=random.randint(1, accounts[0])
+            )
         )
-        block = Block(
-            prev_block=prev_block,
-            transactions=[
-                txn2
-            ],
-            reward=self.reward,
-            difficulty=self.difficulty
+        last_block = blockchain.chain[-1]
+        blockchain.mine(self.wm.public_key)
+        new_last_block = blockchain.chain[-1]
+        self.assertEqual(new_last_block.prev_block, last_block)
+        self.assertEqual(last_block.next_block, new_last_block)
+
+    def test_mine_fails_with_invalid_block(self):
+        blockchain, accounts, wallets = generate_blockchain(length=3, n_transactions=1, n_users=3)
+        txn = generate_signed_transaction(
+            wallets[0],
+            wallets[1],
+            amount=random.randint(1, accounts[0])
         )
-        chain = [prev_block, block]
-        block_chain = BlockChain(chain=chain, reward=self.reward, difficulty=self.difficulty)
-        block_chain.mine(self.wm.public_key)
+        txn.signature = b'0x0'
+
+        blockchain.add_transaction(txn)
+        last_block = blockchain.chain[-1]
+        blockchain.mine(self.wm.public_key)
+        new_last_block = blockchain.chain[-1]
+        self.assertEqual(new_last_block, last_block)
