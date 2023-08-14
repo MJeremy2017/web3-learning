40c5c112a663fa361743b8b68ff012c13cb8f7d7
yue.zhang <zhangyue9306@gmail.com>
2023-08-14 10:44:27 +0800

    added server client

---
+++ blockchain/client.py
@@ -0,0 +1,35 @@
+from utils import unpickle_wallets, unpickle_accounts, generate_signed_transaction
+import random
+import requests
+
+HOST = 'http://localhost'
+PORT = 5000
+
+
+def add_transaction(sender: str, receiver: str, signature: str, amount: int):
+    data = {
+        "sender": sender,
+        "receiver": receiver,
+        "signature": signature,
+        "amount": amount
+    }
+    url = HOST + ":" + str(PORT) + "/transaction/new"
+    resp: requests.Response = requests.post(url, json=data)
+    if resp.status_code == 200:
+        print("Successfully added transaction")
+    else:
+        raise Exception(f"Error adding transaction {resp.text}")
+
+
+if __name__ == '__main__':
+    accounts = unpickle_accounts('accounts')
+    wallets = unpickle_wallets('wallets')
+
+    wa = wallets[0]
+    wb = wallets[1]
+    amount = random.randint(1, accounts[0])
+    txn = generate_signed_transaction(wa, wb, amount)
+    add_transaction(sender=str(txn.from_addr),
+                    receiver=str(txn.to_addr),
+                    signature=str(txn.signature),
+                    amount=txn.amount)
--- blockchain/server.py
+++ blockchain/server.py
@@ -5,24 +5,27 @@ from utils import generate_blockchain, deserialize_public_key, save_accounts_and
 app = Flask(__name__)
 
 
-
 @app.route('/transaction/new', methods=['POST'])
-def add_transaction():
-    data = request.get_json()
-    sender: str = str(data['sender'])
-    receiver: str = str(data['receiver'])
-    amount: int = int(data['amount'])
-    signature: bytes = bytes(data['signature'])
-    txn = Transaction(
-        deserialize_public_key(sender),
-        deserialize_public_key(receiver),
-        amount,
-        signature
-    )
-
-    blockchain.add_transaction(txn)
+def add_new_transaction():
+    try:
+        data = request.get_json()
+        sender: str = str(data['sender'])
+        receiver: str = str(data['receiver'])
+        amount: int = int(data['amount'])
+        signature: bytes = bytes(data['signature'], encoding='utf-8')
+        txn = Transaction(
+            deserialize_public_key(sender),
+            deserialize_public_key(receiver),
+            amount,
+            signature
+        )
+        blockchain.add_transaction(txn)
+        return {'message': "Transaction added successfully"}, 200
+    except Exception as e:
+        return {'error': e}, 502
 
 
 if __name__ == '__main__':
     blockchain, accounts, wallets = generate_blockchain(3, 5, 3, reward=10, difficulty=1)
     save_accounts_and_wallets(accounts, wallets)
+    app.run(debug=True)
